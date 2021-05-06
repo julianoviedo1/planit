2861129f2ccf8e35bed355ffe5047987150808c8
julianoviedo <julianoviedo1@gmail.com>
2021-05-06 16:42:00 -0300

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+User.destroy_all
+Payment.destroy_all
+
+puts 'Creating Users...'
+
+first_user = User.create!(
+  email: 'andrew@mail.com',
+  password: '123456',
+  first_name: 'Andrew',
+  last_name: 'Velez',
+  address: 'Juan Paullier 1018, 11200 Montevideo'
+)
+
+second_user = User.create!(
+  email: 'lolaheadge@mail.com',
+  password: '123456',
+  first_name: 'Lola',
+  last_name: 'Headge',
+  address: 'Pte. Gral. Oscar Gestido 2549, 11300 Montevideo'
+)
+
+puts 'Creating Payments...'
+
+Payment.create!(
+  payment_type: 'debit_card',
+  user: first_user,
+  card_number: 4_110_002_223_335_566,
+  secure_number: 345
+)
+
+Payment.create!(
+  payment_type: 'cash',
+  user: second_user
+)
+
+puts 'Creating Services...'
+  
