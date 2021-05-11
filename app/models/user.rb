ed55a92183de054a733d6c5b87a5c0c6164d91c1
julianoviedo <julianoviedo1@gmail.com>
2021-05-11 10:02:20 -0300

    Add validation to phone number

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,5 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :address, presence: true
+  validates :phone_number, presence: true, length: { in: 8..12, allow_blank: true }
 end
