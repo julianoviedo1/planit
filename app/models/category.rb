aa5ba4a0eaf0d3f2e55c886811c089be9bcdb7f7
julianoviedo <julianoviedo1@gmail.com>
2021-05-13 11:08:08 -0300

    Modified models through association

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
   has_many :service_categories, dependent: :destroy
-  has_many :services, through: :service_category
+  has_many :services, through: :service_categories
 
   validates :name, presence: true
 end
