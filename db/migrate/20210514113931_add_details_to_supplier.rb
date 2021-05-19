0092160c113a6d3f6c378286a3b2b150916c1537
julianoviedo <julianoviedo1@gmail.com>
2021-05-19 14:51:15 -0300

    Added suppliers columns

--- db/migrate/20210514113931_add_details_to_supplier.rb
+++ db/migrate/20210514113931_add_details_to_supplier.rb
@@ -2,5 +2,7 @@ class AddDetailsToSupplier < ActiveRecord::Migration[6.0]
   def change
     add_column :suppliers, :delivery, :boolean
     add_column :suppliers, :offers, :string
+    add_column :suppliers, :opens_at, :integer
+    add_column :suppliers, :close_at, :integer
   end
 end
