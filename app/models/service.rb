ac3e5a0a61ac4cc7c6d99124774750969e8cae86
julianoviedo <julianoviedo1@gmail.com>
2021-05-19 14:53:58 -0300

    Add methods to supplier and photo attr to service

--- app/models/service.rb
+++ app/models/service.rb
@@ -3,6 +3,7 @@ class Service < ApplicationRecord
   has_many :orders
   has_many :service_categories, dependent: :destroy
   has_many :categories, through: :service_categories
+  has_one_attached :photo
 
   validates :name, presence: true
   validates :description, presence: true
--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -5,12 +5,22 @@ class Supplier < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
   validates :phone_number, presence: true, length: { in: 8..12, allow_blank: true }
+  validates :opens_at, numericality: { in: 0..23 }
+  validates :close_at, numericality: { in: 0..23 }
 
   def delivery?
     delivery
   end
 
   def self.categories(category)
-    joins(services: [:categories]).where({ categories: { id: category.id } }).group(:name, :id)
+    joins(services: [:categories]).where({ categories: { id: category.id } }).group(:name,:id)
+  end
+
+  def self.search(search)
+    if search
+      where('suppliers.name LIKE ?', "%#{search}%")
+    else
+      all
+    end
   end
 end
