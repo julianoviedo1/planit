03e4fed0c786eb1d9378450dc44d81ba7c838d20
julianoviedo <julianoviedo1@gmail.com>
2021-05-24 13:22:43 -0300

    Modified order controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,23 @@
 class OrdersController < ApplicationController
   def index
   end
+
+
+  def new
+    Order.new
+  end
+
+  def create
+    Order.new()
+  end
+  
+  private
+
+  def order_params
+    params.require(:categories).permit(:id, :name, :description, :query)
+  end
+
+  def set_order
+    @order = Order.find(params[:id])
+  end
 end
