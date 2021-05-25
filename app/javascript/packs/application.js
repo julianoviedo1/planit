a07276818a4ef74066432f7de89721c72477e924
julianoviedo <julianoviedo1@gmail.com>
2021-05-25 16:38:41 -0300

    Modified to Single quotes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,24 +7,24 @@ require("jquery")
 
 $( document ).ready(function() {
   
-  $("#substract").on('click', () => {
-    $("#quantity")[0].value --
-    const total = parseInt($("#quantity")[0].value) * parseInt($("#servicePrice")[0].innerText.split(' ').pop())
-    $("#total")[0].innerText = ''
-    $("#total")[0].insertAdjacentText('beforeend', `$ ${total}`)
+  $('#substract').on('click', () => {
+    $('#quantity')[0].value --
+    const total = parseInt($('#quantity')[0].value) * parseInt($('#servicePrice')[0].innerText.split(' ').pop())
+    $('#total')[0].innerText = ''
+    $('#total')[0].insertAdjacentText('beforeend', `$ ${total}`)
   })
 
-  $("#add").on('click', () => {
-    $("#quantity")[0].value ++
-    const total = parseInt($("#quantity")[0].value) * parseInt($("#servicePrice")[0].innerText.split(' ').pop())
-    $("#total")[0].innerText = ''
-    $("#total")[0].insertAdjacentText('beforeend', `$ ${total}`)
+  $('#add').on('click', () => {
+    $('#quantity')[0].value ++
+    const total = parseInt($('#quantity')[0].value) * parseInt($('#servicePrice')[0].innerText.split(' ').pop())
+    $('#total')[0].innerText = ''
+    $('#total')[0].insertAdjacentText('beforeend', `$ ${total}`)
   })
 
-  $("#quantity").change(() => {
-    const total = parseInt($("#quantity")[0].value) * parseInt($("#servicePrice")[0].innerText.split(' ').pop())
-    $("#total")[0].innerText = ''
-    $("#total")[0].insertAdjacentText('beforeend', `$ ${total}`)
+  $('#quantity').change(() => {
+    const total = parseInt($('#quantity')[0].value) * parseInt($('#servicePrice')[0].innerText.split(' ').pop())
+    $('#total')[0].innerText = ''
+    $('#total')[0].insertAdjacentText('beforeend', `$ ${total}`)
   })
 });
 
