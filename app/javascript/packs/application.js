fff2c596260ecac4fbeda63082ad6c99dbc18eba
julianoviedo <julianoviedo1@gmail.com>
2021-05-25 16:27:18 -0300

    Modified logic (first step for making an order)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,3 +3,28 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
+
+
+$( document ).ready(function() {
+  
+  $("#substract").on('click', () => {
+    $("#quantity")[0].value --
+    const total = parseInt($("#quantity")[0].value) * parseInt($("#servicePrice")[0].innerText.split(' ').pop())
+    $("#total")[0].innerText = ''
+    $("#total")[0].insertAdjacentText('beforeend', `$ ${total}`)
+  })
+
+  $("#add").on('click', () => {
+    $("#quantity")[0].value ++
+    const total = parseInt($("#quantity")[0].value) * parseInt($("#servicePrice")[0].innerText.split(' ').pop())
+    $("#total")[0].innerText = ''
+    $("#total")[0].insertAdjacentText('beforeend', `$ ${total}`)
+  })
+
+  $("#quantity").change(() => {
+    const total = parseInt($("#quantity")[0].value) * parseInt($("#servicePrice")[0].innerText.split(' ').pop())
+    $("#total")[0].innerText = ''
+    $("#total")[0].insertAdjacentText('beforeend', `$ ${total}`)
+  })
+});
+
