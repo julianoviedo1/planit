d357c68b85a6c3d5113c206bec8105b5ca672ebb
julianoviedo <julianoviedo1@gmail.com>
2021-05-26 15:49:28 -0300

    Modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   get '/', to: 'categories#index'
-  resources :orders, only: %i[index show]
   resources :users, only: %i[edit update]
   resources :categories, only: [:show]
   resources :suppliers, only: [:show]
-  resources :services, only: [:show] do
-    resources :orders, only: %i[new create]
-  end
+  resources :services, only: [:show]
+  resources :orders, only: %i[create index show edit update]
 end
