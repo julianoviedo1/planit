b6e1c4e0ff2de79f5b2509f62597f4266b54e70c
julianoviedo <julianoviedo1@gmail.com>
2021-05-26 15:48:31 -0300

    MOdified order model validations

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,8 +4,8 @@ class Order < ApplicationRecord
   belongs_to :payment
   belongs_to :service
 
-  validates :start_time, numericality: { in: 0..23 }
-  validates :end_time, numericality: { in: 0..23 }
+  validates :start_time, numericality: { in: 0..23 }, on: :update
+  validates :end_time, numericality: { in: 0..23 }, on: :update
   validates :status, presence: true, inclusion: { in: statuses.keys }
-  validates :address, presence: true
+  validates :address, presence: true, on: :update
 end
