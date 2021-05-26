06510f2e1d2e5aae4dc29c37dc208c7589727d37
julianoviedo <julianoviedo1@gmail.com>
2021-05-26 09:05:36 -0300

    Added totalPrice method

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,26 +5,25 @@ require("channels")
 require("jquery")
 
 
-$( document ).ready(function() {
-  
-  $('#substract').on('click', () => {
-    $('#quantity')[0].value --
+$(document).ready(function() {
+  function totalPrice() {
     const total = parseInt($('#quantity')[0].value) * parseInt($('#servicePrice')[0].innerText.split(' ').pop())
     $('#total')[0].innerText = ''
     $('#total')[0].insertAdjacentText('beforeend', `$ ${total}`)
+  }
+
+  $('#substract').on('click', () => {
+    $('#quantity')[0].value --
+    totalPrice()
   })
 
   $('#add').on('click', () => {
     $('#quantity')[0].value ++
-    const total = parseInt($('#quantity')[0].value) * parseInt($('#servicePrice')[0].innerText.split(' ').pop())
-    $('#total')[0].innerText = ''
-    $('#total')[0].insertAdjacentText('beforeend', `$ ${total}`)
+    totalPrice()
   })
 
   $('#quantity').change(() => {
-    const total = parseInt($('#quantity')[0].value) * parseInt($('#servicePrice')[0].innerText.split(' ').pop())
-    $('#total')[0].innerText = ''
-    $('#total')[0].insertAdjacentText('beforeend', `$ ${total}`)
+    totalPrice()
   })
 });
 
