3649a330f327d692daceaf06f117d714e182355c
julianoviedo <julianoviedo1@gmail.com>
2021-05-27 08:50:39 -0300

    Modified Js custom file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,26 +4,3 @@ require("@rails/activestorage").start()
 require("channels")
 require("jquery")
 
-
-$(document).ready(function() {
-  function totalPrice() {
-    const total = parseInt($('#quantity')[0].value) * parseInt($('#servicePrice')[0].innerText.split(' ').pop())
-    $('#total')[0].innerText = ''
-    $('#total')[0].insertAdjacentText('beforeend', `$ ${total}`)
-  }
-
-  $('#substract').on('click', () => {
-    $('#quantity')[0].value --
-    totalPrice()
-  })
-
-  $('#add').on('click', () => {
-    $('#quantity')[0].value ++
-    totalPrice()
-  })
-
-  $('#quantity').change(() => {
-    totalPrice()
-  })
-});
-
